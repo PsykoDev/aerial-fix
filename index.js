14cd57ef76cefb38f70cdf962543e6a61cafa3fe
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2019-03-13 22:11:36 +0100

    Fix maybe useless

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-module.exports = function sign(dispatch) {
+module.exports = function sign(mod) {
 	let gameId;
 	
-	dispatch.hook('S_LOGIN', 9, (event) => {
+	mod.hook('S_LOGIN', 12, (event) => {
 		gameId = event.gameId;
 	});
 	
-	dispatch.hook('S_AIR_REACTION_END', 1, (event) => {
+	mod.hook('S_AIR_REACTION_END', 1, (event) => {
 		if(gameId.toString() == event.gameId.toString()) {
 			event.loc.z += 2;
 			return true;
